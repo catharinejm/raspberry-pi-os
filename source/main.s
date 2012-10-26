6057dde1f7a0614ff4139ab048034518763c1df2
Jon Distad <jon.distad@gmail.com>
2012-10-26 13:57:13 -0400

    Blinkity blink!

--- source/main.s
+++ source/main.s
@@ -3,6 +3,7 @@
 _start:
 
 ldr r0,=0x20200000 @ ADDR of GPIO reg
+mov r2,#0x3F0000 @ Wait timer
 
 mov r1,#1
 lsl r1,#18
@@ -12,8 +13,20 @@ str r1,[r0,#4] @ enable GPIO pin 16
 
 mov r1,#1
 lsl r1,#16
-str r1,[r0,#40]
 
 loop$:
+str r1,[r0,#40]
+bl wait1$
+
+str r1,[r0,#28]
+bl wait1$
+
 b loop$
+
+wait1$:
+sub r2,#1
+cmp r2,#0
+bne wait1$
+mov r2,#0x3F0000 @ Wait timer
+mov pc,lr
 
