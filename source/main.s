d6f611d2f0c13b8746db65a6ce79145d3aeafb2c
Jon Distad <jon.distad@gmail.com>
2012-10-26 12:55:24 -0400

    Turn on OK light!

---
+++ source/main.s
@@ -0,0 +1,19 @@
+.section .init
+.globl _start
+_start:
+
+ldr r0,=0x20200000 @ ADDR of GPIO reg
+
+mov r1,#1
+lsl r1,#18
+str r1,[r0,#4] @ enable GPIO pin 16
+               @ Pins 10-19 in second GPIO DWORD
+               @ 10 + 6th bit triplet starts at 18
+
+mov r1,#1
+lsl r1,#16
+str r1,[r0,#40]
+
+loop$:
+b loop$
+
