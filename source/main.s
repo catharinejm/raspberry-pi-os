f76dd507fc7d403b0d5f27489926dfb511d4caf0
Jon Distad <jon.distad@gmail.com>
2012-10-26 14:57:11 -0400

    Blinking with functions!

--- source/main.s
+++ source/main.s
@@ -1,32 +1,52 @@
 .section .init
 .globl _start
+
 _start:
 
-ldr r0,=0x20200000 @ ADDR of GPIO reg
-mov r2,#0x3F0000 @ Wait timer
+b main
 
-mov r1,#1
-lsl r1,#18
-str r1,[r0,#4] @ enable GPIO pin 16
-               @ Pins 10-19 in second GPIO DWORD
-               @ 10 + 6th bit triplet starts at 18
+.section .text
+main:
+mov sp,#0x8000
 
-mov r1,#1
-lsl r1,#16
+pinNum .req r0
+pinFunc .req r1
+mov pinNum,#16
+mov pinFunc,#1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
 
-loop$:
-str r1,[r0,#40]
-bl wait1$
+mainLoop$:
 
-str r1,[r0,#28]
-bl wait1$
+  pinNum .req r0
+  pinVal .req r1
+  mov pinNum,#16
+  mov pinVal,#0
+  bl SetGpio
+  .unreq pinNum
+  .unreq pinVal
 
-b loop$
+  mov r0,#0x3F0000
+  bl wait$
 
-wait1$:
-sub r2,#1
-cmp r2,#0
-bne wait1$
-mov r2,#0x3F0000 @ Wait timer
-mov pc,lr
+  pinNum .req r0
+  pinVal .req r1
+  mov pinNum,#16
+  mov pinVal,#1
+  bl SetGpio
+  .unreq pinNum
+  .unreq pinVal
 
+  mov r0,#0x3F0000
+  bl wait$
+
+  b mainLoop$
+
+wait$:
+  cycles .req r0
+  sub cycles,#1
+  cmp cycles,#0
+  bne wait$
+  .unreq cycles
+  mov pc,lr
