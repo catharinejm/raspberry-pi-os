fde7d1fa4364cdedb939c1cd04d1bed99a185dbd
Jon Distad <jon.distad@gmail.com>
2012-11-02 13:14:29 -0400

    WIP: Morse Code

--- source/main.s
+++ source/main.s
@@ -17,30 +17,37 @@ bl SetGpioFunction
 .unreq pinNum
 .unreq pinFunc
 
+ptrn .req r4
+ldr ptrn,=pattern
+ldr ptrn,[ptrn]
+seq .req r5
+mov seq,#0
+
 mainLoop$:
-
   pinNum .req r0
   pinVal .req r1
   mov pinNum,#16
-  mov pinVal,#0
-  bl SetGpio
-  .unreq pinNum
-  .unreq pinVal
 
-  
-  ldr r0,=1000000
-  bl Sleep
-
-  pinNum .req r0
-  pinVal .req r1
-  mov pinNum,#16
   mov pinVal,#1
+  lsl pinVal,seq
+  and pinVal,ptrn
+
   bl SetGpio
   .unreq pinNum
   .unreq pinVal
 
+  add seq,#1
+  and seq,#31
+  
   ldr r0,=1000000
   bl Sleep
 
   b mainLoop$
 
+.unreq ptrn
+.unreq seq
+
+.section .data
+.align 2
+pattern:
+.int 0b11111111101010100010001000101010
